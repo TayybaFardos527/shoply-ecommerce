75b8557579c92514d98391cf1811c3750709fb88
Tayyba Fardos <tayybafardos527@gmail.com>
2026-08-10 14:20:06 -0700

    Connect frontend to live backend URL

--- ecommerce-store/frontend/src/api/axios.js
+++ ecommerce-store/frontend/src/api/axios.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: '/api' })
-
+const api = axios.create({ baseURL: 'https://backend-kk7brszxu-tayyba1.vercel.app/api' })
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
   if (token) config.headers.Authorization = `Bearer ${token}`
